26a42646d3cd86bb64f4b4cfb4dbcefa3c67bd30
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2024-09-27 14:25:07 -0400

    Refactor function return types to ensure consistency with input types

--- src/dnsRequest.py
+++ src/dnsRequest.py
@@ -1,47 +1,50 @@
 from random import randint
 from dnsCommonTypes import recordType
 
+
 class dnsRequest:
     # Note that the UDP packets are encoded in big-endian (network byte order)
 
     # Labels cannot exceed 63 octets
     MAX_LABEL_SIZE = 63
 
-    def __init__(self, domain_name : str, q_type : recordType):
-        self.ID = randint(0,2**16-1)
+    def __init__(self, domain_name: str, q_type: recordType):
+        self.ID = randint(0, 2**16 - 1)
         self.domain_name = domain_name
         self.q_type = q_type
-    
+        self.encoded_question = None
+        self.header = None
+
     def get_encoded_request(self):
         header = self.get_header()
         encoded_question = self.get_encoded_question()
         return header + encoded_question
-    
+
     def get_header(self) -> bytes:
         # Return the already computed instance variable if existant
-        if hasattr(self, 'header'):
+        if hasattr(self, "header"):
             return self.header
-        header = b''
-        header += self.ID.to_bytes(2)  #Add the ID at the very top of the header
-        # Adding the second line of the header which specifies 
+        header = b""
+        header += self.ID.to_bytes(2)  # Add the ID at the very top of the header
+        # Adding the second line of the header which specifies
         # QR = 0, Opcode = 0000, AA = 0, TC = 0, RD = 1, RA = 0, Z = 000, RCODE = 0000 (in order)
         header += (0b0000000100000000).to_bytes(2)
-        # Adding the third line of the header which specifies 
+        # Adding the third line of the header which specifies
         # QDCOUNT = 0x0001 always in our case
-        header += b'\x00\x01'
+        header += b"\x00\x01"
         # Finally, ANCOUNT, NSCOUNT, and ARCOUNT are all 0x0000 because this is a request message
-        header += 6*b'\x00'
+        header += 6 * b"\x00"
 
-        #Update the instance variable
+        # Update the instance variable
         self.header = header
         return header
 
     def get_encoded_question(self) -> bytes:
         # Return the already computed instance variable if existant
-        if hasattr(self, 'encoded_question'):
+        if hasattr(self, "encoded_question"):
             return self.encoded_question
 
-        question = b''
+        question = b""
         # Get the ecnoding for QNAME
         question += self.get_QNAME_encoding()
 
@@ -50,25 +53,27 @@ class dnsRequest:
         # For our project, QCLASS is always 0x0001
         question += (0x0001).to_bytes(2)
 
-        #Update the instance variable
+        # Update the instance variable
         self.encoded_question = question
         return question
 
     def get_QNAME_encoding(self):
         # Fetch labels from domain name by splitting at '.' -> www.mcgill.ca = ['www', 'mcgill', 'ca']
-        labels = self.domain_name.split('.')
+        labels = self.domain_name.split(".")
 
-        q_name = b''
+        q_name = b""
 
         for label in labels:
             # Get the length of the label
             len_label = len(label)
             # Make sure that labels are less than 63 Octets long
             if len(label) > 63:
-                raise ValueError(f'Label {label} in doemain name is too long (labels are restricted to {dnsRequest.MAX_LABEL_SIZE} octets max)')
+                raise ValueError(
+                    f"Label {label} in domain name is too long (labels are restricted to {dnsRequest.MAX_LABEL_SIZE} octets max)"
+                )
             # add the length + the label ascii encoding
-            q_name += len_label.to_bytes() + label.encode('ascii')
+            q_name += len_label.to_bytes() + label.encode("ascii")
 
         # Terminates with zero-length octet
-        q_name += b'\x00'
+        q_name += b"\x00"
         return q_name
--- src/dnsResponse.py
+++ src/dnsResponse.py
@@ -1,5 +1,6 @@
 from dnsCommonTypes import *
 from typing import NamedTuple
+from typing import Tuple
 
 
 class dnsHeader(NamedTuple):
@@ -19,7 +20,7 @@ class MX_record(NamedTuple):
 
 
 class dnsAnswer(NamedTuple):
-    NAME: tuple
+    NAME: str
     TYPE: recordType
     # TODO: ask what QCODE is. It doesn't appear in question packet description
     CLASS: int
@@ -32,12 +33,14 @@ class dnsResponse:
     def __init__(self, message: bytes):
         self._message = message
         self.ID = dnsResponse.parseID(self.message)
-        self.parseMessage(message)
+        self.parseMessage(self.message)
+        self.header = None
+        self.answers = None
 
     @property
-    def message(self):
+    def message(self) -> bytes:
         return self._message
-    
+
     @staticmethod
     def parseID(message: bytes) -> int:
         return int.from_bytes(message[:2])
@@ -51,7 +54,7 @@ class dnsResponse:
         answer_section_end = answer_section_start + self.header.ANCOUNT
         answer_section = message[answer_section_start:answer_section_end]
         self.answers = dnsResponse.parseAnswerSection(
-            answer_section, self.header.ANCOUNT
+            answer_section, self.header.ANCOUNT, self.message
         )
 
     @staticmethod
@@ -69,57 +72,67 @@ class dnsResponse:
         return dnsHeader(QR, OPCODE, AA, TC, RA, RCODE, QDCOUNT, ANCOUNT)
 
     @staticmethod
-    def parseAnswerSection(answer_section: bytes, ANCOUNT: int) -> NamedTuple:
+    def parseAnswerSection(
+        answer_section: bytes, ANCOUNT: int, message: bytes
+    ) -> list[dnsAnswer]:
         import pdb
 
         pdb.set_trace()
         unparsed_answer_section = answer_section
         answers = []
         for i in range(ANCOUNT):
-            NAME, idx = dnsResponse.parseName(unparsed_answer_section)
-            unparsed_answer_section = unparsed_answer_section[idx:]  # Update unparsed section variable
+            NAME, idx = dnsResponse.parseName(unparsed_answer_section, message)
+            unparsed_answer_section = unparsed_answer_section[
+                idx:
+            ]  # Update unparsed section variable
             TYPE = recordType(int.from_bytes(unparsed_answer_section[:2]))
             CLASS = int.from_bytes(unparsed_answer_section[2:4])
             TTL = int.from_bytes(unparsed_answer_section[4:8])
             RDLENGTH = int.from_bytes(unparsed_answer_section[8:10])
-            RDATA = dnsResponse.parse_RDATA(unparsed_answer_section[10 : 10 + RDLENGTH], TYPE)
+            RDATA = dnsResponse.parse_RDATA(
+                unparsed_answer_section[10 : 10 + RDLENGTH], TYPE, message
+            )
             answers.append(dnsAnswer(NAME, TYPE, CLASS, TTL, RDLENGTH, RDATA))
         return answers
 
     @staticmethod
-    def parseName(answer: bytes):
+    def parseName(answer: bytes, message) -> Tuple[str, int]:
         import pdb
+
         pdb.set_trace()
-        
+
         idx = 0
         labels = []
-        if check_pointer(answer[idx].to_bytes()):
+        if check_pointer(answer[idx]):
             offset = get_pointer_value(answer[idx : idx + 2])
-            offset_labels = dnsResponse.extract_value_at_pointer(dnsResponse.message, offset)
+            offset_labels = extract_value_at_pointer(message, offset)
             labels.extend(offset_labels)
-            idx += 2 # Skip Pointer
+            idx += 2  # Skip Pointer
         else:
             while answer[idx] != 0:
-        
+
                 label_length = answer[idx]
                 idx += 1  # Skip over label length byte
                 label = answer[idx : idx + label_length].decode("ascii")
                 labels.append(label)
                 idx += label_length
             idx += 1  # To skip over the terminating character
-        return (".".join(labels), idx)
-    
+        return ".".join(labels), idx
+
     @staticmethod
-    def parse_RDATA(answer: bytes, record_type: recordType) -> IPV4 | str | MX_record:
-        match (recordType):
+    def parse_RDATA(
+        answer: bytes, record_type: recordType, message
+    ) -> IPV4 | str | MX_record:
+        match recordType:
             case recordType.A:
                 return IPV4(answer[0], answer[1], answer[2], answer[3])
             case recordType.NS | recordType.CNAME:
                 # TODO: Does the name of the alias also have the same format as QNAME?
-                return dnsResponse.parseName(answer)
+                return dnsResponse.parseName(answer, message)[0]
             case recordType.MX:
                 return MX_record(
-                    int.from_bytes(answer[:2]), dnsResponse.parseName(answer[2:])
+                    int.from_bytes(answer[:2]),
+                    dnsResponse.parseName(answer[2:], message)[0],
                 )
 
 
@@ -132,15 +145,16 @@ def get_range_bit(byte: int, start_index: int, end_index: int) -> int:
     return (byte >> start_index) & (2 ** (end_index - start_index + 1) - 1)
 
 
-def check_pointer(byte: bytes) -> bool:
+def check_pointer(byte: int) -> bool:
     # check that it starts with 0b11000000
     return (byte & 0b11000000) == 0b11000000
 
 
-def get_pointer_value(pointer: bytes) -> int:
+def get_pointer_value(pointer: int) -> int:
     return pointer & 0b0011111111111111
 
-def extract_value_at_pointer(message : bytes, pointer_offset: int) -> list[str]:
+
+def extract_value_at_pointer(message: bytes, pointer_offset: int) -> list[str]:
     labels = []
     while message[pointer_offset] != 0:
         label_length = message[pointer_offset]
